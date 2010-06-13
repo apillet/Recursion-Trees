a90823cedf2ed872fc8a78c49e588fbd05c1caeb
Ariel H. Pillet <apillet@gmail.com>
2010-06-13 02:04:01 -0300

    Removes unused code.

--- basic_recursion_tree.rb
+++ basic_recursion_tree.rb
@@ -14,17 +14,6 @@ module Common
   
 end
 
-class Numeric 
-  def gosu_to_radians
-    (self - 90) * Math::PI / 180.0
-  end
-  
-  def radians_to_gosu
-    self * 180.0 / Math::PI + 90
-  end
-
-end
-
 include Common
 
 class Tree
